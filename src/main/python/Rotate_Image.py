a03222af682894e0b811e4b482b47fce149a898a
Jiehan Zhu <jessie.zhu@connectbyamfam.com>
2020-10-08 08:43:10 -0500

    Rotate Image

--- src/main/python/Rotate_Image.py
+++ src/main/python/Rotate_Image.py
@@ -7,6 +7,7 @@ You have to rotate the image in-place, which means you have to modify the input 
 another 2D matrix and do the rotation.
 """
 
+
 class Solution(object):
     def rotate(self, matrix):
         """
@@ -14,20 +15,30 @@ class Solution(object):
         :rtype: None Do not return anything, modify matrix in-place instead.
         """
         n = len(matrix)
-        i = 0
-        j = 0
-        for j in range(n):
-            prev = matrix[i][j]
-            matrix[j][n - 1], prev = prev, matrix[j][n - 1]
-            matrix[n - 1][n - 1 - j], prev = prev, matrix[n - 1][n - 1 - j]
-            matrix[n - 1][-j], prev = prev, matrix[n - 1][n - 1 - j]
-            matrix[n - 1][-j], prev = prev, matrix[j][n - 1]
+        for i in range(int(n / 2)):
+            for j in range(i, n - 1 - i):
+                prev = matrix[i][j]
+                cnt = 0
+                while cnt < 4:
+                    i_next = j
+                    j_next = n - 1 - i
+                    matrix[i_next][j_next], prev = prev, matrix[i_next][j_next]
+                    i = i_next
+                    j = j_next
+                    cnt += 1
+                # print(f'i, j = {i, j}')
+                # for r in matrix:
+                #     print(r)
 
-            print(matrix, '\n', prev)
-            if i == 0:
-                j_next = n - 1
-            prev = matrix[i][j]
+        for r in matrix:
+            print(r)
 
 
-matrix = [[1,2,3],[4,5,6],[7,8,9]]
-[[7,4,1],[8,5,2],[9,6,3]]
+sol = Solution()
+test = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+sol.rotate(test)
+assert test == [[7, 4, 1], [8, 5, 2], [9, 6, 3]]
+
+test = [[5, 1, 9, 11], [2, 4, 8, 10], [13, 3, 6, 7], [15, 14, 12, 16]]
+sol.rotate(test)
+assert test == [[15, 13, 2, 5], [14, 3, 4, 1], [12, 6, 8, 9], [16, 7, 10, 11]]
