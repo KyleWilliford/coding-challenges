868764a10bda18bfc85eff8b31f32465f439ecd0
Jiehan Zhu <jessie.zhu@connectbyamfam.com>
2020-10-12 14:49:21 -0500

    add regular expression solutions

--- src/main/python/String/Count_and_Say.py
+++ src/main/python/String/Count_and_Say.py
@@ -19,19 +19,21 @@ other words from the previous member read off the digits, counting the number of
 Note: Each term of the sequence of integers will be represented as a string.
 """
 import itertools
+import re
 
 
 class Solution:
-    def countAndSayRecursive(self, n: int) -> str:
+    def countAndSay_iterative(self, n: int) -> str:
         """
-        recursive
+        iterative
 
         Runtime: 36 ms. Your runtime beats 60.75 % of python3 submissions.
         Memory Usage: 14.3 MB
         :param n:
         :return:
         """
-        def transferToSay(count: str):
+        count = '1'
+        for j in range(n-1):
             say = []
             cnt = 1
             key = count[0]
@@ -46,26 +48,84 @@ class Solution:
                 # print(key, cnt)
             say.append(str(cnt))
             say.append(key)
-            return ''.join(say)
-
-        count = '1'
-        for j in range(n-1):
-            count = transferToSay(count)
+            count = ''.join(say)
 
         return count
 
-    def countAndSay_2(self, n: int) -> str:
-        ans = '1'
-        for _ in range(n - 1):
-            v = ''
-            for digit, group in itertools.groupby(ans):
-                count = len(list(group))
-                v += (str(count) + digit)
-            ans = v
-        return ans
+    def countAndSay_recursive(self, n: int) -> str:
+        """
+        recursive
+
+        Runtime: 36 ms. Your runtime beats 60.75 % of python3 submissions.
+        Memory Usage: 14.3 MB
+        :param n:
+        :return:
+        """
+
+        if n == 1:
+            return '1'
+
+        count = self.countAndSay_Recursive(n-1)
+        say = []
+        cnt = 1
+        key = count[0]
+        for i in range(1, len(count)):
+            if count[i] == key:
+                cnt += 1
+            else:
+                say.append(str(cnt))
+                say.append(key)
+                cnt = 1
+                key = count[i]
+            # print(key, cnt)
+        say.append(str(cnt))
+        say.append(key)
+        return ''.join(say)
+
+    def countAndSay_RErecursive(self, n: int) -> str:
+        """
+        Recursive Regular Expression
+        https://leetcode-cn.com/problems/count-and-say/solution/duo-chong-fang-fa-qiu-jie-wai-guan-shu-lie-bu-fang/
+
+        :param n:
+        :return:
+        """
+        if n == 1:
+            return '1'
+
+        s = self.countAndSay_RE(n-1)
+        p = r'(\d)\1*'
+        pattern = re.compile(p)
+        res = [_.group() for _ in pattern.finditer(s)]  # 提取结果
+        return ''.join(str(len(c)) + c[0] for c in res)  # join 内部的 str(len(c)) + c[0] for c in res 是生成器类型
+
+    def countAndSay_REiterative(self, n: int) -> str:
+        """
+        Iterative Regular Expression
+        https://leetcode-cn.com/problems/count-and-say/solution/duo-chong-fang-fa-qiu-jie-wai-guan-shu-lie-bu-fang/
+
+        :param n:
+        :return:
+        """
+        res = '1'
+        p = r'(\d)\1*'
+        pattern = re.compile(p)
+        for _ in range(n-1):
+            res = pattern.sub(lambda x: str(len(x.group())) + x.group(1), res)  # 替换
+        return res
+
+
+
+
 
 
 sol = Solution()
-n = 2
-sol.countAndSay(n)
-sol.countAndSay_2(n)
+n = 6
+assert sol.countAndSay_Iteration(2) == '11'
+assert sol.countAndSay_Recursive(2) == '11'
+assert sol.countAndSay_Iteration(6) == '312211'
+assert sol.countAndSay_Recursive(6) == '312211'
+assert sol.countAndSay_Iteration(10) == '13211311123113112211'
+assert sol.countAndSay_Recursive(10) == '13211311123113112211'
+assert sol.countAndSay_RE(6) == '312211'
+assert sol.countAndSay_REInplace(6) == '312211'
