fdbc884b720f31143c1742f496c8f85fa201287a
Jiehan Zhu <jessie.zhu@connectbyamfam.com>
2020-10-13 10:32:45 -0500

    add reverse linked list

---
+++ src/main/python/Linked_List/Reverse Linked List.py
@@ -0,0 +1,37 @@
+"""
+https://leetcode.com/explore/interview/card/top-interview-questions-easy/93/linked-list/560/
+
+Reverse a singly linked list.
+
+Example:
+Input: 1->2->3->4->5->NULL
+Output: 5->4->3->2->1->NULL
+Follow up:
+A linked list can be reversed either iteratively or recursively. Could you implement both?
+"""
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList_interative(self, head: ListNode) -> ListNode:
+        """
+        Runtime: 32 ms. Your runtime beats 89.08 % of python3 submissions.
+        Memory Usage: 15.3 MB
+        :param head:
+        :return:
+        """
+        priorNode = None
+        currNode = head
+        while currNode:
+            nextNode = currNode.next
+            currNode.next = priorNode
+            priorNode = currNode
+            currNode = nextNode
+
+        return priorNode
+
+    def reverseList_resursive(self, head: ListNode) -> ListNode:
