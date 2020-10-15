bbd554e2fde3cbc7e67bed56be5c896d9632caac
Jiehan Zhu <jessie.zhu@connectbyamfam.com>
2020-10-15 13:02:28 -0500

    merge two sorted linst

--- src/main/python/Linked_List/Merge Two Sorted Lists.py
+++ src/main/python/Linked_List/Merge Two Sorted Lists.py
@@ -36,3 +36,32 @@ class Solution:
 
         print(f'head = {head}')
         return head
+
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        print(f'l1 = {l1}, l2 = {l2}')
+        if not l1:
+            return l2
+        if not l2:
+            return l1
+        if l1.val <= l2.val:
+            head = l1
+        else:
+            head = l2
+        print(f'head = {head}')
+        tail = head
+        print(f'tail = ={tail}')
+        while l1 and l2:
+            if l1.val <= l2.val:
+                tail.next = l1
+                l1 = l1.next
+                print(f'l1 = {l1}, l2 = {l2}')
+            else:
+                tail.next = l2
+                l2 = l2.next
+            print(f'head, tail = {head, tail}, l1 = {l1}, l2 = {l2}')
+        if l1:
+            tail.next = l1
+        elif l2:
+            tail.next = l2
+        print(f'head, tail = {head, tail}, l1 = {l1}, l2 = {l2}')
+        return head
